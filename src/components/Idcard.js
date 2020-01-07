55899b11fddf2d57c8963ee37fe22d21ff88485f
waynezdev <cas021919@coderacademy.edu.au>
2020-01-07 15:24:30 +1100

    first commit

---
+++ src/components/Idcard.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Idcard extends Component {
+
+    render () {
+        return (
+            <>
+            <h1>ID card</h1>
+            <div>
+
+                avatar place holder
+            </div>
+
+            <div>
+                username
+
+            </div>
+
+            <div>
+                email
+                
+            </div>
+
+            </>
+        )
+        }
+
+}
+
+
+
+export default Idcard
